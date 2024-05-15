05b1db3e263a5da213d36a816e79f28ccf2745a1
walifrontendengineer <wali.frontend-engineer@itecexperts.com>
2024-05-15 20:36:59 +0500

    Javascript objects

---
+++ JavaScript/loops.js
@@ -0,0 +1,33 @@
+const arr = [0, 1, 2, 3, 4, 5, 6];
+
+for (let key = 1; key < 5; key++) {
+  //   console.log("indexes", key);
+}
+
+// while loop
+let key = 0;
+while (key < arr.length) {
+  //   console.log("Statement running" + " " + key);
+  key++;
+}
+
+const person = {
+  name: "John",
+  age: 20,
+  address: "UK",
+};
+
+for (let key in person) {
+  //   console.log(`${key}: ${person[key]}`);
+}
+
+for (value of arr) {
+  //   console.log(value);
+}
+
+const countOccurence = (string, char) => {
+  return;
+};
+
+const str = "Welcome to Javascript, aw ws ba pata lage makhke ta";
+countOccurence(str, "a");
---
+++ JavaScript/object.js
@@ -0,0 +1,75 @@
+// Object
+const person = {
+  name: "Iqrar",
+  age: 25,
+  height: "5 ft & 6 inc",
+  address: "123 Main Street",
+  profession: {
+    job: "Web Developer",
+    salary: 50000,
+    company: "Google Inc",
+  },
+  walk: (name) => {
+    return `${person.name} is walking`;
+  },
+};
+
+person.age = 40;
+person.name = "Asif";
+delete person.address;
+person.walk = () => {
+  return "This is chnaged function";
+};
+person.name = "Wali Ullah";
+// console.log(person["profession"]["job"]);
+
+// Creating an object with constructor function
+function Employee() {
+  this.name = "Asif";
+  this.age = 40;
+  this.address = "Peshawar";
+  this.profession = "Software Engineer";
+}
+
+const e1 = new Employee();
+// console.log(e1);
+
+// Factory function
+const person1 = (name) => {
+  return {
+    name,
+    age: 20,
+    address: "Peshawar",
+    isWalking: false,
+    study: () => {
+      return "Ali is studing in BS Software Engineering";
+    },
+  };
+};
+
+const p1 = person1("Ali");
+const p2 = { ...p1 };
+const p3 = Object.assign({ profession: "Student" }, p2);
+
+// Object Getter & Setter
+
+const circle = {
+  size: 50,
+  radius: 7,
+  area: 200,
+  get getArea() {
+    return this.area;
+  },
+
+  set changeSize(val) {
+    this.area = val;
+  },
+};
+
+// Hoisting Problem
+
+s();
+
+const s = function sum() {
+  console.log(10 + 20);
+};
